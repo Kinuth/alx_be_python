e03d12756b510f5ee9bed89b8c6577724e2300d6
Kinuth <kinuthiaryan01@gmail.com>
2025-09-10 10:15:59 +0300

    basic python operations

--- basic_operations.py
+++ basic_operations.py
@@ -1,8 +1,9 @@
 number1 = 10
-number2 = 5
-result1 = number1 + number2
-result2 = number1 - number2
-result3 = number1 * number2
-print("Addition of", number1, "and", number2, "is:", result1)
-print("Subtraction of", number1, "and", number2, "is:", result2)  
-print("Multiplication of", number1, "and", number2, "is:", result3)
+number2 = 5 
+sum_result = number1 + number2
+difference_result = number1 - number2
+product_result = number1 * number2
+
+print("Addition of", number1, "and", number2, "is:", sum_result)
+print("Subtraction of", number1, "and", number2, "is:", difference_result)
+print("Multiplication of", number1, "and", number2, "is:", product_result)
