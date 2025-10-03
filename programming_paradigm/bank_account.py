bd0151187798e74fbbb0238ae86597bf27199dbb
Kinuth <kinuthiaryan01@gmail.com>
2025-10-03 21:02:31 +0300

    Defined a class BankAccount

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -15,5 +15,5 @@ class BankAccount:
         return False
 
     def display_balance(self):
-        print(f"Current Balance: ${self.balance}")
+        print(f"Current Balance: ${self.balance:.2f}")
         return self.balance
